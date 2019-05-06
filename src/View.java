c8c6748edba3eb0dc623dab1466b4ef688c79848
Bo Johnson <yobo.johnson@gmail.com>
2019-05-06 13:39:51 -0700

    Added Rought Sudoku Grid to the View

--- src/View.java
+++ src/View.java
@@ -2,27 +2,59 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
+import javafx.scene.layout.*;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 
 public class View extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+		int numCols = 9;
+		int numRows = 9;
+		int cellSize = 40;
+
 		primaryStage.setTitle("My First JavaFX App");
+		primaryStage.setHeight(500);
+		primaryStage.setWidth(500);
 
-		Label label = new Label("Hello World");
-		Scene scene = new Scene(label, 400, 200);
+
+		GridPane gridPane = new GridPane();
+		gridPane.setHgap(1);
+		gridPane.setVgap(1);
+		gridPane.setPadding(new Insets(10, 10, 10, 10));
+		gridPane.setAlignment(Pos.CENTER);
+		gridPane.setGridLinesVisible(true);
+		
+		//gridPane.setHgap(25);
+
+		// Setitng up the grid for Sudoku
+		for(int i = 0; i < numCols; i++) {
+			gridPane.getColumnConstraints().add(new ColumnConstraints(cellSize));
+		}
+
+		for(int i = 0; i < numRows; i++) {
+			gridPane.getRowConstraints().add(new RowConstraints(cellSize));
+		}
+
+		for(int i = 0; i < numCols; i++) {
+			for(int j = 0; j < numRows; j++) {
+				/*
+				Label label = new Label("1");
+				GridPane.setConstraints(label, i, j);
+				gridPane.getChildren().add(label);
+				*/
+				gridPane.add(new Label("1"), i, j);
+			}
+		}
+
+		Scene scene = new Scene(gridPane);
 		primaryStage.setScene(scene);
-
 		primaryStage.show();
 
-		/*
-		// Making and showing a second stage from scratch
-		Label label2 = new Label("This is the second stage");
-		Scene scene2 = new Scene(label2, 400, 200);
-		Stage stage2 = new Stage();
-		stage2.setTitle("This is the second stage");
-		stage2.setScene(scene2);
-		stage2.show();
-		*/
 	}
 
 	public static void main(String[] args) {
