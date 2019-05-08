8b8db677067b915f39bc6b1d862d6b4b33daec65
Bo Johnson <yobo.johnson@gmail.com>
2019-05-07 17:39:17 -0700

    Finished Rendering the Grid

--- src/View.java
+++ src/View.java
@@ -21,6 +21,7 @@ public class View extends Application {
 		int numCols = 9;
 		int numRows = 9;
 		int cellSize = 120;
+		int innerCellSize = cellSize / 3;
 
 		primaryStage.setTitle("SudokuFX");
 		primaryStage.setHeight(winHeight);
@@ -30,6 +31,9 @@ public class View extends Application {
 		int outerGridCols = 3;
 		int outerGridRows = outerGridCols;
 		
+		int innerGridCols = 3;
+		int innerGridRows = innerGridCols;
+
 		GridPane outerGrid = new GridPane();
 		outerGrid.setPadding(new Insets(10, 10, 10, 10));
 		outerGrid.setAlignment(Pos.CENTER);
@@ -44,15 +48,58 @@ public class View extends Application {
 
 		// Adding the empty panes
 		Pane[] squares = new Pane[9];
+		GridPane[] innerGrids = new GridPane[9];
+		Pane[][] cells = new Pane[9][9];
 
-		for(int i = 0; i < squares.length; i++) {
-			squares[i] = new Pane(new Label((i + 1) + ""));
-			squares[i].getStyleClass().add("square-" + (i + 1));
+		// initalize all the cells
+		int cellIndex = 0;
+		for(int i = 0; i < cells.length; i++) {
+			for(int j = 0; j < cells[0].length; j++) {
+				cells[i][j] = new Pane();
+				cells[i][j].getStyleClass().add("inner-grid");
+			}
 		}
 
+		// Initalizing each square and adding a grid to it
+		for(int i = 0; i < squares.length; i++) {
+			squares[i] = new Pane();
+			squares[i].getStyleClass().add("square-" + (i + 1));
 
+			innerGrids[i] = new GridPane();
+
+			for(int j = 0; j < innerGridCols; j++) {
+				innerGrids[i].getColumnConstraints().add(new ColumnConstraints(innerCellSize));
+			}
+
+			for(int j = 0; j < innerGridRows; j++) {
+				innerGrids[i].getRowConstraints().add(new RowConstraints(innerCellSize));
+			}
+
+			//innerGrids[i].setGridLinesVisible(true);
+			squares[i].getChildren().addAll(innerGrids[i]);
+		}
+
+		// Adding the cells to the inner grids in the squares
+		int rowOffset = 0;
+		int colOffset = 0;
+
+		for(int i = 0; i < innerGrids.length; i++) {
+			for(int j = 0; j < innerGridRows; j++) {
+				for(int k = 0; k < innerGridCols; k++) {
+					innerGrids[i].add(cells[j + rowOffset][k + colOffset], k, j);
+				}
+			}
+
+
+			if (i == 2 || i == 5) {
+				rowOffset += 3;
+			}
+			colOffset = (colOffset + 3) % 9;
+		}
+		
+
+		// Adding the squares ot the outer grid
 		int count = 0;
-
 		for(int i = 0; i < outerGridCols; i++) {
 			for(int j = 0; j < outerGridRows; j++) {
 				outerGrid.add(squares[count++], j, i);
