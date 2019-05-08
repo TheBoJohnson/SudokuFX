375a0fea35e89ddcf42bce7baf3deed7a923d41c
Bo Johnson <yobo.johnson@gmail.com>
2019-05-08 09:51:44 -0700

    Refactored the start method

--- src/View.java
+++ src/View.java
@@ -15,96 +15,20 @@ import javafx.geometry.HPos;
 public class View extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		int winHeight = 500;
+		// Setting up the Main Window
+		int winHeight = 1000;
 		int winWidth = 1000;
-
-		int numCols = 9;
-		int numRows = 9;
-		int cellSize = 120;
-		int innerCellSize = cellSize / 3;
 
 		primaryStage.setTitle("SudokuFX");
 		primaryStage.setHeight(winHeight);
 		primaryStage.setWidth(winWidth);
 
-		// The outer grid of the Sudoku grid that will be 3x3
-		int outerGridCols = 3;
-		int outerGridRows = outerGridCols;
-		
-		int innerGridCols = 3;
-		int innerGridRows = innerGridCols;
-
 		GridPane outerGrid = new GridPane();
-		outerGrid.setPadding(new Insets(10, 10, 10, 10));
-		outerGrid.setAlignment(Pos.CENTER);
-
-		for(int i = 0; i < outerGridCols; i++) {
-			outerGrid.getColumnConstraints().add(new ColumnConstraints(cellSize));
-		}
-
-		for(int i = 0; i < outerGridRows; i++) {
-			outerGrid.getRowConstraints().add(new RowConstraints(cellSize));
-		}
-
-		// Adding the empty panes
-		Pane[] squares = new Pane[9];
+		Pane[] outerCells = new Pane[9];
 		GridPane[] innerGrids = new GridPane[9];
-		Pane[][] cells = new Pane[9][9];
+		Pane[][] innerCells = new Pane[9][9];
 
-		// initalize all the cells
-		int cellIndex = 0;
-		for(int i = 0; i < cells.length; i++) {
-			for(int j = 0; j < cells[0].length; j++) {
-				cells[i][j] = new Pane();
-				cells[i][j].getStyleClass().add("inner-grid");
-			}
-		}
-
-		// Initalizing each square and adding a grid to it
-		for(int i = 0; i < squares.length; i++) {
-			squares[i] = new Pane();
-			squares[i].getStyleClass().add("square-" + (i + 1));
-
-			innerGrids[i] = new GridPane();
-
-			for(int j = 0; j < innerGridCols; j++) {
-				innerGrids[i].getColumnConstraints().add(new ColumnConstraints(innerCellSize));
-			}
-
-			for(int j = 0; j < innerGridRows; j++) {
-				innerGrids[i].getRowConstraints().add(new RowConstraints(innerCellSize));
-			}
-
-			//innerGrids[i].setGridLinesVisible(true);
-			squares[i].getChildren().addAll(innerGrids[i]);
-		}
-
-		// Adding the cells to the inner grids in the squares
-		int rowOffset = 0;
-		int colOffset = 0;
-
-		for(int i = 0; i < innerGrids.length; i++) {
-			for(int j = 0; j < innerGridRows; j++) {
-				for(int k = 0; k < innerGridCols; k++) {
-					innerGrids[i].add(cells[j + rowOffset][k + colOffset], k, j);
-				}
-			}
-
-
-			if (i == 2 || i == 5) {
-				rowOffset += 3;
-			}
-			colOffset = (colOffset + 3) % 9;
-		}
-		
-
-		// Adding the squares ot the outer grid
-		int count = 0;
-		for(int i = 0; i < outerGridCols; i++) {
-			for(int j = 0; j < outerGridRows; j++) {
-				outerGrid.add(squares[count++], j, i);
-			}
-		}
+		setUpGrid(outerGrid, outerCells, innerGrids, innerCells);
 
 		// Outermost holder layout
 		HBox holder = new HBox();
@@ -115,6 +39,90 @@ public class View extends Application {
 		primaryStage.setScene(scene);
 		primaryStage.show();
 
+	}
+	public void setUpGrid(GridPane outerGrid, Pane[] outerCells, GridPane[] innerGrids, Pane[][] innerCells) {
+		int cellSize = 200;
+		int innerCellSize = cellSize / 3;
+
+		setupOuterGrid(outerGrid, cellSize);
+		setupOuterCells(outerCells);
+		setupInnerGrids(innerGrids, innerCellSize);
+		setupInnerCells(innerCells);
+
+		// adding the innerGrids to the outerCells
+		for(int i = 0; i < outerCells.length; i++) {
+			outerCells[i].getChildren().addAll(innerGrids[i]);
+		}
+
+		// adding the innerCells to the innerGrids
+		int innerGridDimen = 3;
+		int rowOffset = 0;
+		int colOffset = 0;
+
+		for(int i = 0; i < innerGrids.length; i++) {
+			for(int j = 0; j < innerGridDimen; j++) {
+				for(int k = 0; k < innerGridDimen; k++) {
+					innerGrids[i].add(innerCells[j + rowOffset][k + colOffset], k, j);
+				}
+			}
+
+			if (i == 2 || i == 5) {
+				rowOffset += 3;
+			}
+			colOffset = (colOffset + 3) % 9;
+		}
+
+		// adding outerCells to the outerGrid
+		int outerGridDimen = 3;
+		int outerCellCount = 0;
+
+		for(int i = 0; i < outerGridDimen; i++) {
+			for(int j = 0; j < outerGridDimen; j++) {
+				outerGrid.add(outerCells[outerCellCount++], j, i);
+			}
+		}
+	}
+
+	public void setupOuterGrid(GridPane outerGrid, int cellSize) {
+		int outerGridDimen = 3;
+
+		outerGrid.setPadding(new Insets(10, 10, 10, 10));
+		outerGrid.setAlignment(Pos.CENTER);
+
+		for(int i = 0; i < outerGridDimen; i++) {
+			outerGrid.getColumnConstraints().add(new ColumnConstraints(cellSize));
+			outerGrid.getRowConstraints().add(new RowConstraints(cellSize));
+		}
+	}
+
+
+	public void setupInnerGrids(GridPane[] innerGrids, int innerCellSize) {
+		int innerGridDimen = 3;
+
+		for(int i = 0; i < innerGrids.length; i++) {
+			innerGrids[i] = new GridPane();
+
+			for(int j = 0; j < innerGridDimen; j++) {
+				innerGrids[i].getColumnConstraints().add(new ColumnConstraints(innerCellSize));
+				innerGrids[i].getRowConstraints().add(new RowConstraints(innerCellSize));
+			}
+		}
+	}
+
+	public void setupOuterCells(Pane[] outerCells) {
+		for(int i = 0; i < outerCells.length; i++) {
+			outerCells[i] = new Pane();
+			outerCells[i].getStyleClass().add("square-" + (i + 1));
+		}
+	}
+
+	public void setupInnerCells(Pane[][] innerCells) {
+		for(int i = 0; i < innerCells.length; i++) {
+			for(int j = 0; j < innerCells[0].length; j++) {
+				innerCells[i][j] = new Pane();;
+				innerCells[i][j].getStyleClass().add("inner-grid");
+			}
+		}
 	}
 
 	public static void main(String[] args) {
